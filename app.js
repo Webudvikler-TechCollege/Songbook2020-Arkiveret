365b32283ca3b0e95751fb2eef59d02a6baa7b11
Heinz K <heka@techcollege.dk>
2020-01-23 13:34:16 +0100

    Updates

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+//Sørger for at vi kan læse post variabler
 app.use(express.urlencoded({extended:true}));
 
+//Sætter til 4242 hvis default port ikke er angivet
 const port = process.env.PORT || 4242;
 
 //Require config & route files
